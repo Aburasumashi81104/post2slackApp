5f8ade61d30bde5e362f29baf4e9bbc9bdc24e25
Aburasumashi81104 <boostedzombie@gmail.com>
2018-04-03 16:54:49 +0900

    save tokenへの画面遷移実装

--- app/src/main/java/com/streaningworks/slackmimick/MainActivity.kt
+++ app/src/main/java/com/streaningworks/slackmimick/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.streaningworks.slackmimick
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "tokenbutton")
+        val theme: Button = findViewById<View>(R.id.settoken) as Button
+        theme.setOnClickListener{
+           val intent = Intent(this, SaveTokenActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
