a8f7d7e01dad2f9588ef6522a998648510c937ec
Aburasumashi81104 <boostedzombie@gmail.com>
2018-04-13 15:31:12 +0900

    POSTボタン内にメソッド記述

--- app/src/main/java/com/streaningworks/slackmimick/MainActivity.kt
+++ app/src/main/java/com/streaningworks/slackmimick/MainActivity.kt
@@ -6,21 +6,63 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
+    private var inputChannel: EditText? = null
+    private var inputName: EditText? = null
+    private var inputText: EditText? = null
+    private var inputIcon: EditText? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //tokenボタン押下時の処理
         Log.d(TAG, "tokenbutton")
-        val theme: Button = findViewById<View>(R.id.settoken) as Button
-        theme.setOnClickListener{
+        val token: Button = findViewById<View>(R.id.settoken) as Button
+        token.setOnClickListener{
            val intent = Intent(this, SaveTokenActivity::class.java)
             startActivity(intent)
         }
 
+        inputChannel = findViewById(R.id.input_channel)
+        inputName = findViewById(R.id.input_name)
+        inputText = findViewById(R.id.input_text)
+        inputIcon = findViewById(R.id.input_icon)
+
+        //postボタン押下時の処理
+        Log.d(TAG, "postbutton")
+        val post: Button = findViewById<View>(R.id.posturl) as Button
+        post.setOnClickListener{
+
+            getChannelName()
+            getCharName()
+            getSendText()
+            getIconUrl()
+
+        }
+
+    }
+
+    private fun getChannelName(){
+        inputChannel?.getText().toString()
+    }
+    private fun getCharName(){
+        inputChannel?.getText().toString()
+    }
+    private fun getSendText(){
+        inputText?.getText().toString()
+    }
+    private fun getIconUrl(){
+        inputIcon?.getText().toString()
     }
 }
--- app/src/main/java/com/streaningworks/slackmimick/SaveTokenActivity.kt
+++ app/src/main/java/com/streaningworks/slackmimick/SaveTokenActivity.kt
@@ -2,11 +2,78 @@ package com.streaningworks.slackmimick
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_save_token.*
+import android.R.id.edit
+import android.content.Context
+import android.widget.TextView
+import com.streaningworks.slackmimick.R.id.text
+import java.io.FileNotFoundException
+import java.io.IOException
+
 
 class SaveTokenActivity : AppCompatActivity() {
+
+    private val fileName : String = "testfile.txt"
+//   private var dataStore: SharedPreferences? = null
+//   private var inputToken: EditText? = null
+//   private val textWrite: TextView? = null
+    val textRead: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_token)
+
+        savebutton.setOnClickListener({
+                val text : String = input_token.getText().toString()
+                saveFile(fileName,text)
+                if(text.isEmpty()){
+                    text_view.setText(R.string.no_text)
+                } else {
+                    text_view.setText(R.string.saved)
+                }
+
+        })
+
+        readbutton.setOnClickListener({
+
+            var str : String = readFile(fileName)
+            if(str.isEmpty()) text_view.setText(R.string.read_error)
+            else text_view.setText(str)
+
+        })
+
     }
+
+    //"testfile.txt"にtokenを保存
+    fun saveFile(file : String,str : String){
+        try {
+            val out = openFileOutput(file,Context.MODE_PRIVATE)
+            out.write(str.toByteArray())
+            out.close()
+        } catch (e: IOException){
+            e.printStackTrace()
+
+        }
+    }
+
+    //"testfile.txt"に保存されたtokenを表示
+    fun readFile(file : String) : String {
+        var text : String = ""
+        try{
+            val reader = openFileInput(file).bufferedReader()
+            for(lineBuffer in reader.readLines()){
+                text += lineBuffer.toString()
+            }
+            reader.close()
+        } catch(e: FileNotFoundException){
+            e.printStackTrace()
+        }
+        return text
+    }
+
 }
